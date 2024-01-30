a5f18811c86c4f8fab0a37da897f107386fc4fcb
sergiioog <sergiogargor9@gmail.com>
2024-01-30 14:16:01 +0100

    Conseguimos Sass y emoezamos el proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    plugins: [
+        [
+          '@snowpack/plugin-sass',
+          {
+            /* options */
+          },
+        ],
+      ],
+});
